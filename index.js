910ce24f134eba50486d735ca2ce96d318678ead
MdAfsarHossain <afsar291722@gmail.com>
2024-12-25 23:56:29 +0600

    Index main file added

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://historicgems-e6d80.web.app",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser()); // CookieParser use for to get token from the cookie store.
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b6ov8m0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// Verify Token
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    // Create a new database
+    const artifactsCollection = client
+      .db("historic-gems")
+      .collection("all-artifacts");
+    const likedArtifactsCollection = client
+      .db("historic-gems")
+      .collection("liked-artifacts");
+
+    // JWT Generate
+
+    // Clear token on logout
+
+    // ALL POST requests
+    // Create new artifact data
+
+    // Create new liked artifacts
+
+    // ALL GET requests
+
+    // Top Artifacts
+
+    // Get Single Artifact details
+
+    // Get Specific users all added artifacts
+
+    // Liked artifacts
+
+    // Check Liked
+
+    // ALL PATCH requests
+
+    // ALL Delete Request
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Historics Gems Server site!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
