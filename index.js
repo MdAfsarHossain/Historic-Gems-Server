ddb885b132a0482d366637f0d563ce02ee8ffe34
MdAfsarHossain <afsar291722@gmail.com>
2024-12-25 23:57:55 +0600

    Generate JWT code added

--- index.js
+++ index.js
@@ -66,6 +66,20 @@ async function run() {
       .collection("liked-artifacts");
 
     // JWT Generate
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
     // Clear token on logout
 
